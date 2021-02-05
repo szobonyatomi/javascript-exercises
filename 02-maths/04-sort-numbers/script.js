15a4c14aa60d22d029ba0095f316d328aca3f59c
Tomi <szobonyatamas@gmail.com>
2021-02-05 12:24:58 +0100

    exe array 5.4-5.6

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -17,6 +17,22 @@
 
     var x = document.getElementById('numbers').value;
     var numbers = x.split(',');
+    /*
+ for (var i=0;i<x;i++) {
+            x[i]=parseInt(x[i]);
+        }
+
+        for (var x = 0; x < x - 1; x++) {
+            for (var i = 0; i < x - 1; i++) {
+                if (x[i] > x[i + 1]) {
+                    var temp = x[i + 1];
+                    x[i + 1] = x[i];
+                    x[i] = temp;
+                }
+            }
+        }
+
+ */
 
     alert(
       numbers.sort(function (a, b) {
